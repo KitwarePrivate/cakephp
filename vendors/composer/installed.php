d1e45444b46ad9d53a1c345193a160a20e2438f4
Bryon Bean <bryon.bean@kitware.com>
2024-05-31 22:10:35 -0400

    fix: fixes vendors directory (autoload) ignored by git

---
+++ vendors/composer/installed.php
@@ -0,0 +1,141 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '48ef04b6c2d99a23caabd73af26e1173dabaa11e',
+    'name' => 'kitwareprivate/cakephp',
+  ),
+  'versions' => 
+  array (
+    'cakephp/cakephp-codesniffer' => 
+    array (
+      'pretty_version' => '1.0.4',
+      'version' => '1.0.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0f3559af72fd9c2bba7310a7c4496fbb97502352',
+    ),
+    'kitwareprivate/cakephp' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '48ef04b6c2d99a23caabd73af26e1173dabaa11e',
+    ),
+    'phpstan/phpstan' => 
+    array (
+      'pretty_version' => '1.10.55',
+      'version' => '1.10.55.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9a88f9d18ddf4cf54c922fbeac16c4cb164c5949',
+    ),
+    'phpunit/php-code-coverage' => 
+    array (
+      'pretty_version' => '1.2.18',
+      'version' => '1.2.18.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fe2466802556d3fe4e4d1d58ffd3ccfd0a19be0b',
+    ),
+    'phpunit/php-file-iterator' => 
+    array (
+      'pretty_version' => '1.4.5',
+      'version' => '1.4.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '730b01bc3e867237eaac355e06a36b85dd93a8b4',
+    ),
+    'phpunit/php-text-template' => 
+    array (
+      'pretty_version' => '1.2.1',
+      'version' => '1.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '31f8b717e51d9a2afca6c9f046f5d69fc27c8686',
+    ),
+    'phpunit/php-timer' => 
+    array (
+      'pretty_version' => '1.0.8',
+      'version' => '1.0.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '38e9124049cf1a164f1e4537caf19c99bf1eb260',
+    ),
+    'phpunit/php-token-stream' => 
+    array (
+      'pretty_version' => '1.2.2',
+      'version' => '1.2.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ad4e1e23ae01b483c16f600ff1bebec184588e32',
+    ),
+    'phpunit/phpunit' => 
+    array (
+      'pretty_version' => '3.7.38',
+      'version' => '3.7.38.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '38709dc22d519a3d1be46849868aa2ddf822bcf6',
+    ),
+    'phpunit/phpunit-mock-objects' => 
+    array (
+      'pretty_version' => '1.2.3',
+      'version' => '1.2.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5794e3c5c5ba0fb037b11d8151add2a07fa82875',
+    ),
+    'rector/rector' => 
+    array (
+      'pretty_version' => '0.19.0',
+      'version' => '0.19.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '503f4ead06b3892bd106f67f3c86d4e36c94423d',
+    ),
+    'squizlabs/php_codesniffer' => 
+    array (
+      'pretty_version' => '1.5.6',
+      'version' => '1.5.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6f3e42d311b882b25b4d409d23a289f4d3b803d5',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.23.0',
+      'version' => '1.23.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '46cd95797e9df938fdd2b03693b5fca5e64b01ce',
+    ),
+    'symfony/yaml' => 
+    array (
+      'pretty_version' => 'v2.8.52',
+      'version' => '2.8.52.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '02c1859112aa779d9ab394ae4f3381911d84052b',
+    ),
+  ),
+);
